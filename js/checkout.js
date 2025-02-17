1ed0a43528db0c45790d3064c0dca80124d1e137
Rocio Hsu <shachien@gmail.com>
2025-02-17 17:14:48 +0800

    Update 更新購物車結帳頁
    20250217

--- js/checkout.js
+++ js/checkout.js
@@ -6,4 +6,10 @@ $(function(){
     }else{
         
     }
+
+    //電腦版 手機版 購物車結帳頁外幣換算開合
+    $('.foreign-currency__btn  > a').click(function(){
+        $(this).toggleClass('open');
+        $(this).parent('.foreign-currency__btn ').next('.pay-group__pane').stop(false, true).slideToggle(300);
+    });
 });
